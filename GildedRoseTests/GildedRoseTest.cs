8e8c7700a0d2a539a02633756970566fc0af2311
Panos Anastasiadis <anastapd@gmail.com>
2024-03-14 20:42:46 +0000

    implemented skipping sulfuras items

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -6,12 +6,21 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Test]
-    public void Foo()
+    [TestCase(1, 80, 80)]
+    [TestCase(0, 80, 80)]
+    [TestCase(-1, 80, 80)]
+    public void WhenItemIsSulfuras_ShouldNotUpdateQualityOrSellIn(int sellIn, int quality, int expectedQuality)
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        // Arrange
+        var name = "Sulfuras, Hand of Ragnaros";
+        var items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
         var app = new GildedRose(items);
+
+        // Act
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+
+        // Assert
+        Assert.That(items[0].SellIn, Is.EqualTo(sellIn));
+        Assert.That(items[0].Quality, Is.EqualTo(expectedQuality));
     }
 }
